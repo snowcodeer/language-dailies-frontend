df2f9cddf80c6aeeed55cc62de10b5f26ae4efb1
Natalie <nataliechan1963@gmail.com>
2025-03-07 00:42:45 +0000

    music box added

--- src/components/MusicBox.jsx
+++ src/components/MusicBox.jsx
@@ -9,11 +9,16 @@ const MusicBox = () => {
         <span className="heading-text">Sugerencia de canción de hoy</span>
         <FaMusic style={{ color: '#dd4a4a' }} />
       </h2>
-      <div className="inner-box pastel-red" style={{ display: 'flex', alignItems: 'center', padding: '20px' }}>
-        <div className="inner-box pastel-blue" style={{ color: 'white', display: 'flex', alignItems: 'center', padding: '20px' }}>
-    
+      <div className="inner-box pastel-red" style={{ display: 'flex', alignItems: 'left', padding: '10px' }}>
+        <div className="music-container">
+          <div className="play-button-container">
+            <div className="play-button"></div>
+          </div>
+          <div className="song-details">
+            <div className="song-title neutral-text">Song Title</div>
+            <div className="artist-name grey-text">Artist Name</div>
+          </div>
         </div>
-        
       </div>
     </>
   );
